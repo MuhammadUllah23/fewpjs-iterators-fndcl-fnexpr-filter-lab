3ab4f88a8cb23344ed53cb355acb4a2e7fdd0fcb
Muhammad <muhammadhullah23@gmail.com>
2021-09-12 20:13:03 -0400

    commit tree a5dbbbf37324462a5e97f886f70a93655ecf219f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(taxiDrivers, driver) {   
+    let foundDriver = taxiDrivers.filter(taxiDriver => { 
+    return taxiDriver.toLowerCase() === driver.toLowerCase()
+    });
+    return foundDriver
+    }
+    
+    function fuzzyMatch(taxiDrivers, driver) {
+        return taxiDrivers.filter(taxiDriver => 
+        taxiDriver.charAt(0).toLowerCase() === driver.charAt(0).toLowerCase()
+        );
+        }
+
+
+        function matchName(taxiDrivers, driver) {   
+            let foundDriver = taxiDrivers.filter(taxiDriver => { 
+            return taxiDriver.name.toLowerCase() === driver.toLowerCase()
+            });
+            return foundDriver
+            }
